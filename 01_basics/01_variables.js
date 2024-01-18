41133a7aa28db4560eb531a198358ee125d1ef63
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-18 23:46:56 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const AccountID=14562
+
+let AccountName="Rudransh Pokhriyal"
+var AccountEmail="rudransh12@gmail.com"
+AccountPassword="123456"
+
+AccountID=15345 //Not allowed will generate error as it is a constant
+
+console.table([AccountID,AccountName,AccountEmail,AccountPassword])
