778efee39b075e69130c222b64314feb8e2aaf8d
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-18 18:29:26 +0000

    datatypes in JS

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+//7 types
+let num=19.05
+let bign=23842304120312141n
+let str="Rudransh Pokhriyal"
+let flag=true
+let val
+let symbol=null
+
+console.log(typeof num);
+console.log(typeof bign);
+console.log(typeof str);
+console.log(typeof flag);
+console.log(typeof val);
+console.log(typeof symbol);
+
+console.log("--------------")
+console.log("typeof undefined is "+typeof undefined)
+console.log("typeof null is "+typeof null);
