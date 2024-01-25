4d6d9cf64e23f6d691db5efe9c8748cc57953f7d
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-25 18:29:38 +0000

    Changes made

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -5,4 +5,9 @@ console.log("Value of V is "+v)
 //but we can use placeholders and back ticks ` this is the right and modern way
 console.log(`My name is ${name} and my age is ${v}`);
 
-//if we use new keyword 
+//if we use new keyword then we can create a string which is fundamentally an object with key value pairs where key is indices and value is a character.This gives us the capability to use many inbuilt functions most of which are same as Java but we can use these functions on normal strings declared in variables and constants
+
+let nam=new String("Rudransh Pokhriyal")
+console.log(nam[0])
+console.log(nam.length)
+console.log(nam.charAt(0))
