f50fac08d01a690c56a266db96b1b1bfd32c0005
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-25 18:22:48 +0000

    Strings basics in JS

---
+++ 01_basics/strings.js
@@ -0,0 +1,8 @@
+let v=22
+const name="Rudransh Pokhriyal"
+//to print a variable or const we can use +
+console.log("Value of V is "+v)
+//but we can use placeholders and back ticks ` this is the right and modern way
+console.log(`My name is ${name} and my age is ${v}`);
+
+//if we use new keyword 
