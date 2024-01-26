bda221a65c96a9de6c9b1e8da82f6882e068f2bd
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-26 18:29:11 +0000

    math.random is left

---
+++ 01_basics/math.js
@@ -0,0 +1,16 @@
+//Math is inbuilt library in JS which has similar functions as Java's Math library
+let num=new Number(-3)
+console.log(Math.abs(num))
+
+let deci=new Number(12.6)
+console.log(Math.round(deci));
+console.log(Math.floor(deci));
+console.log(Math.ceil(deci));
+
+let num2=new Number(64)
+console.log(Math.sqrt(num2));
+
+console.log(Math.max(num,deci,num2))
+console.log(Math.min(num,deci,num2))
+
+//Math.random function
