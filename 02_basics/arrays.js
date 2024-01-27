748d62049af76a8fef95ba0cca0f4072119af142
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-27 18:26:04 +0000

    arrays incomplete

---
+++ 02_basics/arrays.js
@@ -0,0 +1,3 @@
+//arrays in JS works similar as in any other language but difference is that here it can have mixed values and it is resizable
+let arr=[1,3,5,"A","C",true,{name:"Rudransh",age:18}]
+console.log(arr)
