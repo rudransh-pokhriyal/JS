90ae2897bd6e98136ea2643b9fdd46cb9d7f8ad5
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-29 18:27:50 +0000

    nested objects freeze combination and Object Functions Destructring and API left

---
+++ 02_basics/objects.js
@@ -0,0 +1,37 @@
+//objects contains key value pairs
+// 2 ways to create object 
+
+//1 way
+const User1={
+    name:"Rudransh Pokhriyal",
+    "locat ion":"dehradun",
+    age:21,
+    isLoggedin:false,
+    LastLoginDays:["monday","friday"]
+}
+
+//2 way
+const User2=new Object(
+{name:"Raj Saxena",
+"locat ion":"delhi",
+age:25,
+isLoggedin:true,
+LastLoginDays:["tuesday","friday","saturday"]})
+
+//Accesing values using keys also has 2 ways
+//Way 1 (.) dot operator
+console.log(User1.name)
+console.log(User1.age)
+
+//Way 2 ["key"]
+console.log(User2["name"]);
+
+//Note if we have made a key in string format i.e "locat ion" then we can only access this using Way 2
+console.log(User1["locat ion"]);
+
+// to use symbol we have to define it in [] in object
+const Sym=Symbol("key")
+const useSymbol={
+    Sym:"KeySymbol"
+}
+console.log(typeof useSymbol["Sym"])// will display value correctly but will not work as symbol 
