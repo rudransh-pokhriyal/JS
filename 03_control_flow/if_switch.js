ea85ec04373bde7eadb3034c2357e1a584069ec9
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-02-07 15:21:38 +0000

    if else and switch in JS

---
+++ 03_control_flow/if_switch.js
@@ -0,0 +1,48 @@
+if('2'===2){
+ console.log("two is equal to two")
+}
+else
+{
+   console.log("two is not equal to two");
+}
+
+//if else if else
+let marks=55
+
+if(marks>=90)
+console.log("A grade");
+
+else if(marks>=75 && marks<=89)
+console.log("B grade");
+
+else if(marks>=55 && marks<=74)
+console.log("C grade");
+
+else
+console.log("Student failed");
+
+//switch case
+
+let val=3
+switch (val) 
+{
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    case 4:
+        console.log("Thursday");
+        break;
+
+    default:
+        console.log("Wrong input")
+        break;
+}
