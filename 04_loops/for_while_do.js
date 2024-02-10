120ffbcb1d177f54b77d4c3203058c4c2cdbc3c2
Rudransh Pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-02-10 16:05:54 +0000

    for while and do while loops

---
+++ 04_loops/for_while_do.js
@@ -0,0 +1,23 @@
+const array=[1,2,false,"rudransh",{age:18}]
+
+// for loop
+for (let index = 0; index < array.length; index++) {
+        console.log(array[index]); 
+}
+
+// while loop
+let i=0;
+while(i<array.length)
+{
+    console.log(array[i]);
+    i++;
+}
+
+// do while loop
+let j=0
+do
+{
+    console.log(array[j]);
+    j++;
+
+}while(j<array.length);
