aae1d8cd64cee31a873b1522902f67cdc8966489
Rudransh Pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-02-22 14:37:06 +0000

    variable name correction

--- 04_loops/filter_map_reduce.js
+++ 04_loops/filter_map_reduce.js
@@ -1,16 +1,16 @@
 let arr=[1,2,3,4,5,6,7,8,9]
 
 //filter is used to retrieve selective data from an array (i.e when on basis on some conditions we need some data)
-let new_nums=arr.filter((num)=> num>5)// we can use arrow function with implicit return for callback fn of filter
+let new_nums1=arr.filter((num)=> num>5)// we can use arrow function with implicit return for callback fn of filter
 
 //alternatives to above
-/* new_nums=arr.filter((num)=>{
+/* new_nums1=arr.filter((num)=>{
     return num>5
 })
 
-new_nums=arr.filter(function (num){
+new_nums1=arr.filter(function (num){
     return num>5
 })
 */
 
-console.log(new_nums)
+console.log(new_nums1)
